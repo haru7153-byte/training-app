d1ebff99b3ed343cb60629fff688163e48bc14cd
haru7153-byte <haru7153@gmail.com>
2026-06-26 22:29:29 +0900

    simple test

--- api/generate-plan.js
+++ api/generate-plan.js
@@ -1,1 +1,3 @@
-module.exports = async function(req, res) { res.status(200).json({ok: true}); }; 
+export default async function handler(req, res) {
+    res.status(200).json({ok: true, key: !!process.env.ANTHROPIC_API_KEY});
+  }
